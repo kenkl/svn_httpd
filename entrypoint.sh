c2020ae4d2ef682ede8c7f91bd63827c3ac0ea91
kenkl <kenkl@mac.com>
2018-01-12 14:25:07 -0500

    tweaks to entrypoint.sh and httpd-svn.conf to actually start repos in the right place

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,15 @@ fi
 # Let's create the basic SVN repos structure
 if [ `ls /svn | wc -l` -eq 0 ]; then
 	cd /svn
-	svnadmin create repos
+	mkdir repos
+	cd repos
+	svnadmin create empty
+	mkdir /tmp/empty
+        echo '#Empty' > /tmp/empty/README.md
+	echo 'Nothing to see here. This file is auto-created when a container from svn_httpd is spun up.' >> /tmp/empty/README.md
+	svn import /tmp/empty file:///svn/repos/empty -m "auto-creation of the empty project"
+	rm -rf /tmp/empty
+	cd /svn
 	chown -R www-data:www-data repos
 	touch /svn/svn-auth-conf
 fi
