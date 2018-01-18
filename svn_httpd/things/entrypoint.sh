1671cd5a04351ad7e1d6612c0312e7598c683c5b
kenkl <kenkl@curie.kenkl.org>
2018-01-17 20:16:12 -0500

    Clean up Dockerfile and entrypoint.sh to remove cruft and reduce the number of layers needed to generate the image

--- svn_httpd/things/entrypoint.sh
+++ svn_httpd/things/entrypoint.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 set -e
 
-# Set timezone for container
-if [ ! -z $TZ ]; then
-	echo $TZ > /etc/timezone && dpkg-reconfigure -f noninteractive tzdata
-fi
-
 # If no config files in /usr/local/apache2/conf, do init copy.
 if [ `ls /usr/local/apache2/conf | wc -l` -eq 0 ]; then
 	cp -r /tmp/httpd-conf/* /usr/local/apache2/conf/
 fi
 
-# Let's create the basic SVN repos structure
+# Let's create the basic SVN repos structure and place some things if they don't alredy exist
 if [ `ls /svn | wc -l` -eq 0 ]; then
 	cd /svn
 	mkdir repos
@@ -19,13 +14,12 @@ if [ `ls /svn | wc -l` -eq 0 ]; then
 	svnadmin create empty
 	mkdir /tmp/empty
         echo '#Empty' > /tmp/empty/README.md
-	echo 'Nothing to see here. This file is auto-created when a container from svn_httpd is spun up.' >> /tmp/empty/README.md
 	svn import /tmp/empty file:///svn/repos/empty -m "auto-creation of the empty project"
 	rm -rf /tmp/empty
 	cd /svn
 	chown -R www-data:www-data repos
 	touch /svn/svn-auth-conf
-	ln -s /usr/local/sbin/mkrepo.sh /svn/mkrepo.sh
+	ln -s /usr/local/bin/mkrepo.sh /svn/mkrepo.sh
 fi
 
 exec "$@"
